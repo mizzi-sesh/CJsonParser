8575c9d630c8f93fb76d810fb3a3fd431c061100
mizzi-sesh <minerva.mizzi@gmail.com>
2024-06-10 17:47:31 +1000

    Basic argument parsing

--- src/ParseJson.c
+++ src/ParseJson.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
-int main ()
+int main (int argc, char *argv[ ])
 {
-  printf("Hello, World!\n");
+  int i; int j;
 
+  printf("\n");
+  
+  for (i = 0; i < argc; i++){
+  
+    for( j = 0; j < strlen(argv[i]); j++){
+
+      char x = argv[i][j];
+    
+        //FILE *fptr;
+        //printf("%d", argc);
+        //printf("%d", arg1_size);
+    
+      printf("%c", x);
+      //fptr = fopen("", "mode");
+
+    }
+    printf("\n");
+
+  }
+  printf("\n");
   return 0;
-
 }
