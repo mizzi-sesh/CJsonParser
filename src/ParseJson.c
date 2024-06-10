7c1fe9afd0539541243d8fe9f52cfb27162ff66e
mizzi-sesh <minerva.mizzi@gmail.com>
2024-06-10 21:32:12 +1000

    Added primitive file parsing logic

--- src/ParseJson.c
+++ src/ParseJson.c
@@ -1,24 +1,42 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main (int argc, char *argv[ ])
 {
-  int i; int j;
 
   printf("\n");
-  
-  for (i = 0; i < argc; i++){
-  
-    for( j = 0; j < strlen(argv[i]); j++){
 
-      char x = argv[i][j];
+  if (argc == 1)
+  {
+    printf("You must include an directory and file path to a valid JSON file as the second argument to the command.\n");
     
-        //FILE *fptr;
-        //printf("%d", argc);
-        //printf("%d", arg1_size);
-    
-      printf("%c", x);
-      //fptr = fopen("", "mode");
+    printf("\n");
+    return -1;
+  }
+  
+  int i; int j;
+  char directory[strlen(argv[1])-1];
+  for (i = 1; i < argc; i++){
+  
+    for( j = 0; j < strlen(argv[i]); j++){ 
+      //printf("%c", argv[i][j]);
+
+      if (i == 1){
+        memset(directory, '\0', sizeof(directory));
+        printf("%s\n", directory);
+        strncpy(directory, argv[i], strlen(argv[i]));
+        printf("%s\n", directory);
+        FILE *fptr;
+        fptr = fopen(directory, "r");
+        if(fptr){
+          while(fgets(directory, strlen(argv[i]), fptr)) {
+            printf("%s", directory);
+          }
+        }
+        fclose(fptr);
+        return 0;
+      }
 
     }
     printf("\n");
@@ -27,3 +45,5 @@ int main (int argc, char *argv[ ])
   printf("\n");
   return 0;
 }
+
+
